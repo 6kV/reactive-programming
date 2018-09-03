16da9ea43815976eed5c07d5cbf3dc4c047a5883
6kV <nizarazu9@hotmail.com>
2018-09-03 23:27:34 +0200

    add example

--- src/main/kotlin/com/nk/FlowableExample.kt
+++ src/main/kotlin/com/nk/FlowableExample.kt
@@ -4,9 +4,18 @@ import io.reactivex.BackpressureStrategy
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
+import java.math.BigDecimal
+import java.util.*
 
 fun main(args: Array<String>) {
-    Observable.range(0,1000)
+
+    val myAggregate = MyAggregate()
+    println(myAggregate.test())
+    myAggregate.value1 = Optional.of(BigDecimal.ONE)
+
+    println(myAggregate.test())
+
+    Observable.fromIterable<MyData>(MyIterable())
             .toFlowable(BackpressureStrategy.DROP)
             .observeOn(Schedulers.computation())
             .subscribeBy {
@@ -16,3 +25,14 @@ fun main(args: Array<String>) {
 
     Thread.sleep(10000)
 }
+
+class MyAggregate {
+
+    var value1 : Optional<BigDecimal> = Optional.empty()
+    var value2 : Optional<BigDecimal> = Optional.empty()
+
+
+    fun test()= value1.orElseGet { BigDecimal.ZERO }.plus(value2.orElseGet { BigDecimal.ZERO })
+}
+
+
